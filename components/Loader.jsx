0b430ec39db83df7060b776754901632d1ffe623
vishnu5104 <knp19cs044@ceknpy.ac.in>
2022-11-22 23:15:18 +0530

    up-loader

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -3,9 +3,10 @@ import Image from 'next/image';
 import images from '../assets';
 
 const Loader = () => (
-  <div className="flexCenter w-full my-4">
-    <Image src={images.loader} alt="loader" width={100} objectFit="contain" />
-  </div>
+    <div className="flexCenter w-full my-4">
+        <h1>Please fo connect your wallet to see</h1>
+        <Image src={images.loader} alt="loader" width={100} objectFit="contain" />
+    </div>
 );
 
 export default Loader;
